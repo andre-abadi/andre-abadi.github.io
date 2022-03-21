308ed0a0359b9f4aa0c186c277cbf0864a0c9c8c
Andre Abadi <121317+andre-abadi@users.noreply.github.com>
2022-03-21 19:45:40 +0800

    rm -rf /

--- README.md
+++
@@ -1,2 +0,0 @@
-# [andre-abadi.github.io](https://andre-abadi.github.io/)
-Personal, Jekyll-based Blog hosted on Github Pages
--- _archive/_content/finished.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-torrentid=$1
-torrentname=$2
-torrentpath=$3
-
-title="$torrentname"
-message="Finished"
-
-xbmc-send -a "Notification($title,$message,10000)"
